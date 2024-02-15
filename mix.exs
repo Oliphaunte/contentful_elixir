d738f38f7c8239216f43aeb1dcfc9a5e93fa42c5
oliphaunte <9771752+Oliphaunte@users.noreply.github.com>
2024-02-15 13:15:30 -0500

    Update mix for hex publish

--- mix.exs
+++ mix.exs
@@ -7,8 +7,28 @@ defmodule ContentfulElixir.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      name: "ContentfulElixir",
+      docs: [
+        main: "ContentfulElixir",
+        extras: ["README.md"]
+      ]
     ]
+  end
+
+  defp description() do
+    "Elixir wrapper for Contentful calls with LiveView rendering components"
+  end
+
+  defp package do
+    %{
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/Oliphaunte/contentful_elixir"},
+      organization: "kredentials"
+    }
   end
 
   # Run "mix help compile.app" to learn about applications.
@@ -21,7 +41,8 @@ defmodule ContentfulElixir.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:req, "~> 0.4.0"}
+      {:req, "~> 0.4.0"},
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
